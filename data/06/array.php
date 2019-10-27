e9623d18ae06635a87e1d387a4a624f1db961b2a
noritama98n <maiko98n@hotmail.co.jp>
2019-10-27 22:52:14 +0900

    06-01

---
+++ data/06/array.php
@@ -0,0 +1,9 @@
+<?php
+$array = array();
+
+$array[] = 'りんご';
+$array[] = 'みかん';
+$array[] = 'ぶどう';
+//var_dump($array);
+echo $array[2];
+?>
---
+++ data/06/array_other.php
@@ -0,0 +1,7 @@
+<?php
+$array1 = array();
+$array2 = array();
+
+$array1 = array('林檎','蜜柑','葡萄');
+$array2 = array('name' => '鈴木', 'hobby' => 'テニス', 'email' => 'sample@sample.com');
+?>
